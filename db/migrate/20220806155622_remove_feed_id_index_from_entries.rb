3d10a096eb3747d8fd0e3b4f9ee596f778a20914
hjkcai <huajingkun1214@gmail.com>
2024-07-05 12:18:41 +0800

    Fix migrations. Ignore trial plans. Support aarch64

--- db/migrate/20220806155622_remove_feed_id_index_from_entries.rb
+++ db/migrate/20220806155622_remove_feed_id_index_from_entries.rb
@@ -2,7 +2,7 @@ class RemoveFeedIdIndexFromEntries < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def up
-    remove_index :entries, :feed_id, algorithm: :concurrently
+    remove_index :entries, name: :index_entries_on_feed_id, algorithm: :concurrently
   end
 
   def down
